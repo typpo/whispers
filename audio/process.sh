c497ff707b503f59f352f22e10df117ddf922f85
Ian Webster <ianw_github@ianww.com>
2021-09-06 18:16:25 -0700

    denoise audio

--- audio/process.sh
+++ audio/process.sh
@@ -3,8 +3,15 @@
 pushd $(dirname $0) &>/dev/null
 cd whispers
 
+echo 'Generating noise profile...'
+sox michael.wav noise-audio.wav trim 0.6 1.5
+sox noise-audio.wav -n noiseprof noise.prof
 for f in *.wav; do
-  sox ${f} "edited/${f}" reverb 100 50 100
+  echo "Processing ${f}..."
+  sox ${f} audio-clean.wav noisered noise.prof 0.3
+  sox audio-clean.wav "edited/${f}" reverb 100 50 100
 done
+
+echo 'Done.'
 
 popd &>/dev/null
